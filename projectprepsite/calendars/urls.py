774d79c1d7da48ee9402dcb36cdc1a1dcd5c4474
edwardwang4340 <edwardwangthebest@gmail.com>
2023-02-23 09:28:03 -0500

    changed button and added upload file page
    need to fix url config

--- projectprepsite/calendars/urls.py
+++ projectprepsite/calendars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.titlePage, name='titlepage')
+    path('', views.titlePage, name='titlepage'),
+    path('', views.uploadFile, name='uploadfile')
 ]
--- projectprepsite/calendars/views.py
+++ projectprepsite/calendars/views.py
@@ -4,3 +4,5 @@ from django.template import loader
 # Create your views here.
 def titlePage(request):
     return render(request, 'calendars/titlepage.html')
+def uploadFile(request):
+    return render(request, 'calendars/uploadfile.html')
