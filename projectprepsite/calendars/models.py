0032b1db051b622fc29ce218d5972daabe69974d
edwardwang4340 <edwardwangthebest@gmail.com>
2023-03-07 09:19:49 -0500

    tried a new upload method
    error: template does not exist

--- projectprepsite/calendars/models.py
+++ projectprepsite/calendars/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class Document(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+
 # Create your models here.
--- projectprepsite/calendars/views.py
+++ projectprepsite/calendars/views.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 import logging
 from django.contrib import messages
 from django import forms
-from calendars.forms import EventsForm
+from calendars.models import Document
+from calendars.forms import DocumentForm
 
 # Create your views here.
 def titlePage(request):
@@ -13,39 +14,26 @@ def titlePage(request):
 def uploadFile(request):
     return render(request, 'calendars/uploadfile.html')
 def upload_csv(request):
-    data = {}
-    if "GET" == request.method:
-        return render(request, "calendars/upload.html", data)
-    # if not GET, then proceed
-    try:
-        csv_file = request.FILES["csv_file"]
-        if not csv_file.name.endswith('.csv'):
-            messages.error(request,'File is not CSV type')
-            return HttpResponseRedirect(reverse("calendars:upload"))
-        #if file is too large, return
-        if csv_file.multiple_chunks():
-            messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
-            return HttpResponseRedirect(reverse("calendars:upload"))
+    # Handle file upload
+    if request.method == 'GET':
+        form = DocumentForm(request.GET, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
 
-        file_data = csv_file.read().decode("utf-8")		
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
 
-        lines = file_data.split("\n")
-        try:
-            form = EventsForm()
-            if form.is_valid():
-                form.save()
-                handle_uploaded_file(csv_file)			
-            else:
-                logging.getLogger("error_logger").error(form.errors.as_json())												
-        except Exception as e:
-            logging.getLogger("error_logger").error(repr(e))					
-            pass
+        else:
+            form = DocumentForm() # A empty, unbound form
 
-    except Exception as e:
-        logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
-        messages.error(request,"Unable to upload file. "+repr(e))
+    # Load documents for the list page
+    documents = Document.objects.all()
 
-    return render(request, 'calendars/upload.html')
+
+    # Render list page with the documents and the form
+    return render(request, 'upload.html', {'documents': documents, 'form': form})
+    
 def handle_uploaded_file(f):
     with open('calendars/upload'+csv_file.name, 'wb+') as destination:
         for chunk in f.chunks():
